f667c76ddedd0f3149965b70891b518155f64c3f
112Nick <mrnickrus@gmail.com>
2018-03-08 13:41:25 +0300

    Initial commit

---
+++ app/src/main/java/ru/mail/park/diskn/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.mail.park.diskn;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
